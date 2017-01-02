a5a86bbe4b0b6357af5322324b0749708d7d745b
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2017-01-03 00:39:17 +0900

    compiler was changed
    NESASM#

--- hack2.asm
+++ hack2.asm
@@ -1,10 +1,20 @@
 
-;	.catbank $1A
-;;	.catbank $1B
-;	.catbank $1C
-;;	.catbank $1D
-;	.catbank $1E
-;;	.catbank $1F
+	.catbank $00
+	.catbank $02
+	.catbank $04
+	.catbank $06
+	.catbank $08
+	.catbank $0A
+	.catbank $0C
+	.catbank $0E
+	.catbank $10
+	.catbank $12
+	.catbank $14
+	.catbank $16
+	.catbank $18
+	.catbank $1A
+	.catbank $1C
+	.catbank $1E
 	.inesprg $10 ;プログラムバンク数
 	.ineschr $00 ;キャラクタバンク数
 	.inesmir 1   ;
