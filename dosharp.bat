f8f40f987aa7575ece460e5680d5f8d8a5663392
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2017-01-03 02:04:32 +0900

    NESASM#を消した
    なんかまだダメっぽいので自前の16KASMでなんとかしよう

--- dosharp.bat
+++
@@ -1,4 +0,0 @@
-del "hack2.nes"
-NesAsmSharp "hack2.asm" >out.txt
-type out.txt
-pause
--- hack2.asm
+++ hack2.asm
@@ -1,20 +1,20 @@
 
-	.catbank $00
-	.catbank $02
-	.catbank $04
-	.catbank $06
-	.catbank $08
-	.catbank $0A
-	.catbank $0C
-	.catbank $0E
-	.catbank $10
-	.catbank $12
-	.catbank $14
-	.catbank $16
-	.catbank $18
-	.catbank $1A
-	.catbank $1C
-	.catbank $1E
+;	.catbank $00
+;	.catbank $02
+;	.catbank $04
+;	.catbank $06
+;	.catbank $08
+;	.catbank $0A
+;	.catbank $0C
+;	.catbank $0E
+;	.catbank $10
+;	.catbank $12
+;	.catbank $14
+;	.catbank $16
+;	.catbank $18
+;	.catbank $1A
+;	.catbank $1C
+;	.catbank $1E
 	.inesprg $10 ;プログラムバンク数
 	.ineschr $00 ;キャラクタバンク数
 	.inesmir 1   ;
@@ -29,4 +29,4 @@
 	.include "src/bank0F.asm"
 	.include "src/patch.asm"
 	
-;	.bank $1F
+	.bank $1F
